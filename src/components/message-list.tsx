e57924516daa468272f14116521f042010ee7624
Antonis Papaioannou <mrantonp@gmail.com>
2024-09-04 00:56:50 +0300

    Refactor message-list.tsx for improved readability and maintainability

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,6 +1,6 @@
-import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
-import { format, isToday, isYesterday } from "date-fns";
-import { Message } from "./message";
+import { GetMessagesReturnType } from '@/features/messages/api/use-get-messages';
+import { format, isToday, isYesterday } from 'date-fns';
+import { Message } from './message';
 
 interface MessageListProps {
   memberName?: string;
@@ -12,14 +12,14 @@ interface MessageListProps {
   loadMore: () => void;
   isLoadingMore: boolean;
   canLoadMore: boolean;
-};
+}
 
 const formatDateLabel = (dateStr: string) => {
   const date = new Date(dateStr);
   if (isToday(date)) return 'Today';
   if (isYesterday(date)) return 'Yesterday';
   return format(date, 'EEEE, MMMM d');
-}
+};
 
 export const MessageList = ({
   memberName,
@@ -30,60 +30,59 @@ export const MessageList = ({
   data,
   loadMore,
   isLoadingMore,
-  canLoadMore
+  canLoadMore,
 }: MessageListProps) => {
-  const groupedMessages = data?.reduce((groups, message) => {
-    const date = new Date(message._creationTime);
-    const dateKey = format(date, "yyyy-MM-dd");
+  const groupedMessages = data?.reduce(
+    (groups, message) => {
+      const date = new Date(message._creationTime);
+      const dateKey = format(date, 'yyyy-MM-dd');
 
-    if (!groups[dateKey]) {
-      groups[dateKey] = [];
-    }
+      if (!groups[dateKey]) {
+        groups[dateKey] = [];
+      }
 
-    groups[dateKey].unshift(message);
-    return groups;
-  }, 
-  {} as Record<string, typeof data>
-);
-
-
-
-
+      groups[dateKey].unshift(message);
+      return groups;
+    },
+    {} as Record<string, typeof data>
+  );
 
   return (
     <div className="flex-1 flex flex-col-reverse pb-4 overflow-y-auto messages-scrollbar">
       {Object.entries(groupedMessages || {}).map(([dateKey, messages]) => (
         <div key={dateKey}>
           <div className="text-center my-2 relative">
-            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300 "/>
+            <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300 " />
             <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
               {formatDateLabel(dateKey)}
             </span>
           </div>
-          {messages.map((message, index) => (
-            <Message 
-              key={message._id}
-              id={message._id} 
-              memberId={message.memberId}
-              authorImage={message.user.image}
-              authorName={message.user.name}
-              isAuthor={false}
-              reactions={message.reactions}
-              body={message.body}
-              image={message.image}
-              updatedAt={message.updatedAt}
-              createdAt={message._creationTime}
-              isEditing={false}
-              setEditingId={() => {}}
-              isCompact={false}
-              hideThreadButton={false}
-              threadCount={message.threadCount}
-              threadImage={message.threadImage}
-              threadTimestamp={message.threadTimestamp}
-            />
-          ))}
+          {messages.map((message, index) => {
+            return (
+              <Message
+                key={message._id}
+                id={message._id}
+                memberId={message.memberId}
+                authorImage={message.user.image}
+                authorName={message.user.name}
+                isAuthor={false}
+                reactions={message.reactions}
+                body={message.body}
+                image={message.image}
+                updatedAt={message.updatedAt}
+                createdAt={message._creationTime}
+                isEditing={false}
+                setEditingId={() => {}}
+                isCompact={false}
+                hideThreadButton={false}
+                threadCount={message.threadCount}
+                threadImage={message.threadImage}
+                threadTimestamp={message.threadTimestamp}
+              />
+            );
+          })}
         </div>
       ))}
     </div>
-  )
+  );
 };
