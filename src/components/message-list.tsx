2921ad85706c7a1973a18c3d7f4b1970afdc23c9
Antonis Papaioannou <mrantonp@gmail.com>
2024-09-04 00:54:25 +0300

    feat: Add Message component and integrate it into MessageList

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,5 +1,6 @@
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 import { format, isToday, isYesterday } from "date-fns";
+import { Message } from "./message";
 
 interface MessageListProps {
   memberName?: string;
@@ -60,9 +61,26 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => (
-            <div key={index}>
-               {JSON.stringify(message)} 
-            </div>
+            <Message 
+              key={message._id}
+              id={message._id} 
+              memberId={message.memberId}
+              authorImage={message.user.image}
+              authorName={message.user.name}
+              isAuthor={false}
+              reactions={message.reactions}
+              body={message.body}
+              image={message.image}
+              updatedAt={message.updatedAt}
+              createdAt={message._creationTime}
+              isEditing={false}
+              setEditingId={() => {}}
+              isCompact={false}
+              hideThreadButton={false}
+              threadCount={message.threadCount}
+              threadImage={message.threadImage}
+              threadTimestamp={message.threadTimestamp}
+            />
           ))}
         </div>
       ))}
---
+++ src/components/message.tsx
@@ -0,0 +1,52 @@
+import { Doc, Id } from "../../convex/_generated/dataModel";
+
+interface MessageProps {
+  id: Id<"messages">;
+  memberId: Id<"members">;
+  authorImage?: string;
+  authorName?: string;
+  isAuthor: boolean;
+  reactions: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+  }>;
+  body: Doc<"messages">["body"];
+  image: string | null | undefined;
+  createdAt: Doc<"messages">["_creationTime"];
+  updatedAt: Doc<"messages">["updatedAt"];
+  isEditing: boolean;
+  isCompact?: boolean;
+  setEditingId: (id: Id<"messages"> | null) => void;
+  hideThreadButton?: boolean;
+  threadCount?: number;
+  threadImage?: string;
+  threadTimestamp?: number;
+};
+
+
+export const Message = ({
+  id,
+  memberId,
+  authorImage,
+  authorName = "Member",
+  isAuthor,
+  reactions,
+  body,
+  image,
+  createdAt,
+  updatedAt,
+  isEditing,
+  isCompact,
+  setEditingId,
+  hideThreadButton,
+  threadCount,
+  threadImage,
+  threadTimestamp,
+}: MessageProps)  => {
+  return (
+    <div>
+      {JSON.stringify(body)}
+    </div>
+  )
+};
