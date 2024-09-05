69ab2fbd256ef798a4320495536a53d2ce9d6da3
Antonis Papaioannou <mrantonp@gmail.com>
2024-09-05 23:37:53 +0300

    refactor: Add conversations module and createOrGet mutation

---
+++ src/app/workspace/[workspaceId]/member/[memberId]/conversation.tsx
@@ -0,0 +1,13 @@
+import { Id } from "../../../../../../convex/_generated/dataModel";
+
+interface ConversationProps {
+  id: Id<'conversations'>;
+};
+
+export const Conversation = ({ id }: ConversationProps) => {
+  return (
+    <div>
+      Conversation {id}
+    </div>
+  );
+};
